8830f2de76a6eae0544f3f2e5af29a61330ba6b3
Derick <derick.tan988@gmail.com>
2019-05-15 12:28:06 +0800

    Add login and register page

---
+++ register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box-shadow text-center register-container">
+            <div class="container">
+                <form action="" method="post">
+                    <h2 class="text-muted">Register with us!</h2>
+                    <div class="form-group">
+                        <label for=""></label>
+                        <input type="text" class="form-control" name="username" placeholder="Enter your username ...">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="password" placeholder="Enter your password ...">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="login">Login</button>
+                    <div style="margin-top: 20px; margin-bottom: 20px;">
+                        <a href="register.php">I don't have an account</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
