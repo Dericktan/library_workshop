b4db50b5a216eb590df160fa6a367335d23501fe
Cindy Adonia <cindy.adonia3@gmail.com>
2019-05-15 12:29:38 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ pages/admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <title>Library</title>
+</head>
+<body>
+	<?php
+		require'navbar.php';
+	?>
+<!-- Content -->
+<div class="container">
+    <h2>This is Admin's Dashboard</h2>
+    <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
+    <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
+    <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+    <p>Some text..</p>
+</div>
+<!-- end of content -->
+</body>
+</html>
---
+++ pages/admin/navbar.php
@@ -0,0 +1,46 @@
+<!-- Side Navbar -->
+<div class="side-navbar">
+	<ul>
+		<li><a href="#">Welcome</a></li>
+		<li><a href="#home">Home</a></li>
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Manage Book</a>
+			<div class="dropdown-content">
+				<a href="#">Add New Book</a>
+				<a href="#">List of Book</a>
+			</div>
+		</li>
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Manage Discussion Room</a>
+			<div class="dropdown-content">
+				<a href="#">Add New Discussion Room</a>
+				<a href="#">List of Discussion Room</a>
+			</div>
+		</li>
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Manage Past Year</a>
+			<div class="dropdown-content">
+				<a href="#">Add New Past Year</a>
+				<a href="#">List of Past Year</a>
+			</div>
+		</li>
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Manage News</a>
+			<div class="dropdown-content">
+				<a href="#">Add News</a>
+				<a href="#">List of News</a>
+			</div>
+		</li>
+		
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Manage Transaction</a>
+			<div class="dropdown-content">
+				<a href="#">Book Booking Request</a>
+				<a href="#">Discussion Room Booking Request</a>
+				<a href="#">Printing Request</a>
+			</div>
+		</li>
+		<li><a href="#">View Feedback</a></li>
+	</ul>
+</div>
+<!-- end of Side-Navbar -->
