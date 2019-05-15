5c828705b5b818053ceaff931de2ff36fbcdb698
Cindy Adonia <cindy.adonia3@gmail.com>
2019-05-15 15:58:57 +0800

    Update for css and book's CRU
    - Added style for table
    - Add margin for admin container
    - Add function for book such as update, get book details
    - Add new file to view list of book

--- core/book.php
+++ core/book.php
@@ -1,5 +1,27 @@
 <?php
     include'config.php';
+    function checkBookNo()
+    {
+        global $con;
+        $book_no = $_POST['book_no'];
+        $select = "SELECT * FROM tb_book where book_no='$book_no'";
+        $check = mysqli_query($con,$select);
+        if($check->num_rows > 0){
+            return false;
+        }
+        return true;
+    }
+
+    function getBook($id)
+    {
+        global $con;
+        $select = "SELECT * FROM tb_book where tb_book.id ='$id'";
+        $check = mysqli_query($con,$select);
+        if($check->num_rows >0){
+            return $check->fetch_assoc();
+        }
+        return false;
+    }
     
     if(isset($_POST['addBook'])){
         global $con;
@@ -20,30 +42,49 @@
                 return true;
             }
             else{
-                // echo "<div class='alert alert-danger text-center'> Duplicate Book No Detected.</div>";
+                echo "<script> alert('Duplicate Book No Detected');</script>";
+                $url = "/pages/admin/index.php?page=BookForm";
+                $link = $baseUrl . $url;
+                echo '<script> window.location.replace("'. $link .'");</script>';
+                return false;
             }    
         }
         else
         {
-            // echo "<div class='alert alert-danger text-center'> Duplicate Book No Detected.</div>";
+            echo "<script> alert('Duplicate Book No Detected');</script>";
+            $url = "/pages/admin/index.php?page=BookForm";
+            $link = $baseUrl . $url;
+            echo '<script> window.location.replace("'. $link .'");</script>';
+            return false;
         }
     }
 
-    function checkBookNo()
-    {
+    if(isset($_POST['updateBook'])){
         global $con;
-        $book_no = $_POST['book_no'];
-        $select = "SELECT * FROM tb_book where book_no='$book_no'";
-        $check = mysqli_query($con,$select);
-        if($check->num_rows > 0){
+        $id = $_POST['id'];
+        $title = $_POST['title'];
+        $author = $_POST['author'];
+        $book_location = $_POST['book_location'];
+        $book_category = $_POST['book_category'];
+        $sql = "UPDATE tb_book set title='$title', author ='$author', book_location='$book_location', book_category='$book_category' where tb_book.id='$id'";
+        $query = mysqli_query($con,$sql);
+        if($query == TRUE){
+            echo "<script> alert('Successfully Updated the Book');</script>";
+            $url = "/pages/admin/index.php?page=BookList";
+            $link = $baseUrl . $url;
+            echo '<script> window.location.replace("'. $link .'");</script>';
+            return true;
+        }
+        else{
+            echo "<script> alert('Fail to update the Book');</script>";
+            $url = "/pages/admin/index.php?page=BookList";
+            $link = $baseUrl . $url;
+            echo '<script> window.location.replace("'. $link .'");</script>';
             return false;
         }
-        return true;
     }
+
     
-    function insertBook(){
-        
-    }
 
 
 ?>
